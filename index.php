2e86d5d43ae1f0768d7b546f2c5831ad3918f151
lefty993 <antoniovinkovic1@gmail.com>
2021-04-19 20:26:22 +0200

    Stabilna verzija

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Početna</title>
+</head>
+<body>
+    <h2>Ovo je početna stranica</h2><hr>
+    <br><br><br>
+
+    <a href="about.php">Pročitaj nesto o nama</a><br><br>
+
+    <a href="privacy.php">Privatnost</a>
+
+</body>
+</html>
---
+++ privacy.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Privatnost</title>
+</head>
+<body>
+    <h2>Ovo je stranica o privatnosti</h2><hr>
+    <br><br><br>
+
+    <h4>Ma kakva privatnost</h4><br><br><br>
+
+    <a href="index.php">Vrati se na početnu</a><br><br>
+
+<a href="about.php">Pročitaj nesto o nama</a>
+
+</body>
+</html>
